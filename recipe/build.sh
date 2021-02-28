7c0dff7de22b4dd5c567d01584f77ddd1a0a9336
Mark Harfouche <mark.harfouche@gmail.com>
2021-02-28 17:53:18 -0500

    void using patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,12 @@
-make -j${CPU_COUNT}
+OBJS="tree.o unix.o html.o xml.o json.o hash.o color.o file.o"
+if [[ $(uname) == "Darwin" ]]; then
+    OBJS="$OBJS strverscmp.o"
+fi
+
+make -j${CPU_COUNT} \
+    prefix=${PREFIX} \
+    CC=${CC} \
+    MANDIR=${PREFIX}/share/man/man1 \
+    OBJS=$OBJS
+
 make install
