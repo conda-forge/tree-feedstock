66c830d3653adba9ab728ec5c3cf9319567d74f6
Jason K. Moore <moorepants@gmail.com>
2023-03-11 17:44:22 +0100

    Avoid using a patch.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,17 @@
-make -j${CPU_COUNT}
+if [[ $(uname) == "Darwin" ]]; then
+	CFLAGS="-O2 -Wall -fomit-frame-pointer -no-cpp-precomp"
+	LDFLAGS=""
+else  # linux
+	CFLAGS="-O4 -pedantic -Wall -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64"
+	LDFLAGS="-s"
+fi
+
+make -j${CPU_COUNT} \
+    prefix=${PREFIX} \
+    CC=${CC} \
+    CFLAGS=$CFLAGS \
+    LDFLAGS="" \
+    MANDIR=${PREFIX}/share/man/man1 \
+    OBJS=$OBJS
+
 make install
