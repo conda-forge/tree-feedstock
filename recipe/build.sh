c47a180c172afb33fb9d8827a14166756876918f
Jason K. Moore <moorepants@gmail.com>
2023-03-12 09:05:19 +0100

    Try different bash quoting scheme.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,14 @@
 if [[ $(uname) == "Darwin" ]]; then
-	FLAGS='CFLAGS="-O2 -Wall -fomit-frame-pointer -no-cpp-precomp"'
+	CFLAGS="-O2 -Wall -fomit-frame-pointer -no-cpp-precomp"
+	LDFLAGS=
 else  # linux
-	FLAGS='CFLAGS="-O4 -pedantic -Wall -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" LDFLAGS="-s"'
+	CFLAGS="-O4 -pedantic -Wall -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64"
+	LDFLAGS="-s"
 fi
 
-make -j${CPU_COUNT} prefix=${PREFIX} CC=${CC} MANDIR=${PREFIX}/share/man/man1 $FLAGS
-
-make install
+make -j{$CPU_COUNT} \
+	prefix=${PREFIX} \
+	CC=${CC} \
+	MANDIR=${PREFIX}/share/man/man1 \
+	CFLAGS="$CFLAGS" \
+	LDFLAGS="$LDFLAGS"
